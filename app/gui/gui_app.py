fb08e9b9cc54eedc6200838988745824e90a7ece
hilkojj <hilkojj@outlook.com>
2018-10-17 15:38:24 +0200

    very nice ui

---
+++ app/gui/gui_app.py
@@ -0,0 +1,46 @@
+import time
+from threading import Thread
+from tkinter import *
+
+from gui.widgets.module_widget import ModuleWidget
+from gui.widgets.modules_list import ModulesList
+from module import Module
+
+
+class GUI(Frame):
+
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.modules_list = ModulesList(self, self.on_module_select)
+        self.module_widget = ModuleWidget(self)
+
+        self.modules_list.pack(side=LEFT, fill=Y)
+        self.module_widget.pack(side=RIGHT, fill=BOTH, expand=True)
+        self.pack(fill=BOTH, expand=True)
+
+    def on_module_select(self, module):
+        self.module_widget.show_module(module)
+
+    def update_modules(self, modules):
+        self.modules_list.update_list(modules)
+
+
+# create the application
+myapp = GUI()
+
+myapp.master.title("App")
+
+def test():
+    time.sleep(1)
+    myapp.update_modules([
+        Module("harry", {"light.", "30%"}),
+        Module("test", {"temp.", 20}),
+        Module("henk", {"temp.", 30}),
+    ])
+
+
+Thread(target=test).start()
+
+# start the program
+myapp.mainloop()
+
---
+++ app/gui/widgets/module_widget.py
@@ -0,0 +1,27 @@
+import tkinter
+
+from gui.widgets.module_config import ModuleConfig
+from gui.widgets.module_data import ModuleData
+
+TITLE_FONT_SIZE = 20
+BG = "#434d5b"
+
+class ModuleWidget(tkinter.Frame):
+
+    def __init__(self, master):
+        super().__init__(master, bg=BG)
+        self.module = None
+        self.title = tkinter.Label(self, text="Select a module...", bg=BG, fg="white", font=("Helvetica", TITLE_FONT_SIZE))
+        self.title.grid(column=0, row=0)
+
+        self.module_data = ModuleData(self)
+        self.module_config = ModuleConfig(self)
+        self.module_data.grid(column=0, row=1, sticky="nesw")
+        self.rowconfigure(1, weight=1)
+        self.module_config.grid(column=0, row=2, sticky="nesw")
+        self.rowconfigure(2, weight=1)
+        self.columnconfigure(0, weight=1)
+
+    def show_module(self, module):
+        self.module = module
+        self.title.config(text=f"Connected module: {module.name}", font=("Helvetica", TITLE_FONT_SIZE))
