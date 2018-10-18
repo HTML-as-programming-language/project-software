ba18f748d393437df16b36a05c9c10bb7d4d597b
hilkojj <hilkojj@outlook.com>
2018-10-18 11:07:31 +0200

    show data

--- app/gui/widgets/module_data.py
+++ app/gui/widgets/module_data.py
@@ -1,13 +1,17 @@
-from tkinter import Frame
+from tkinter import Frame, Label
 
 BG = "#eaeaea"
 
 
 class ModuleData(Frame):
 
-    def __init__(self, master):
+    def __init__(self, master, module):
         super().__init__(master, borderwidth="2", relief="ridge", bg=BG)
-
-    def show_data_for_module(self, module):
-        pass
-
+        data = module.readable_data_dir()
+        row = 0
+        for key in data.keys():
+            keylabel = Label(self, text=key, bg=BG)
+            vallabel = Label(self, text=data[key], bg=BG, fg="#34266b")
+            keylabel.grid(column=0, row=row)
+            vallabel.grid(column=1, row=row)
+            row += 1
