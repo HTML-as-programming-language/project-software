5171a94cc6c9a071ed1b04bebd6ea13fb20d8a2c
hilkojj <hilkojj@outlook.com>
2018-10-18 11:31:14 +0200

    actions

--- app/module.py
+++ app/module.py
@@ -1,13 +1,23 @@
 
 class Module:
 
-    def __init__(self, name, data=None, config=None):
+    def __init__(self, name, data=None, config=None, actions=None):
         self.name = name
         self.data = data if data else {}
         self.config = config if config else {}
+        self.actions = actions if actions else {}
 
-    def readable_data_dir(self):
+    def readable_data_dict(self):
         """
-        Should return a directory full of human readable data
+        Should return a dictionary full of human readable data
         """
         return self.data
+
+    def get_actions(self):
+        """
+        Should return a dictionary with callbacks.
+        eg: {
+            "Do something": callback_func
+        }
+        """
+        return self.actions
