ef05d5010ecaacdc6252983c2f638b220a7b2b18
remi <remi@de180811>
2018-10-19 12:05:57 +0200

    Make the GUI show the attached Arduino's

--- app/app.py
+++ app/app.py
@@ -16,47 +16,21 @@ def test():
     time.sleep(1)
     b = Backend()
     b.client_maintenance()
+
+    modules = []
     for name, client in b.clients.items():
+        modules.append(Module(
+            name, data={"light": "30%"},
+            actions={
+                "open": client.open_hatch,
+                "close": client.close_hatch,
+            }
+        ))
         print(name, client)
 
-    myapp.update_modules([
-        Module(
-            "harry", data={"light": "30%"},
-            actions={
-                "naar boven": lambda: print("doe iets!?!?!"),
-                "doe iets anders": lambda: print("doe iets anders!?!?!")
-            }
-        ),
-        Module(
-            "test", data={"temp.": "20°C"},
-            actions={
-                "doe iets": lambda: print("doe iets!?!?!"),
-                "doe iets anders": lambda: print("doe iets anders!?!?!")
-            }
-
-        ),
-        Module(
-            "henk", data={"temp.": "30°C"},
-            actions={
-                "doe iets": lambda: print("doe iets!?!?!"),
-                "doe iets anders": lambda: print("doe iets anders!?!?!")
-            },
-            config={
-                "temp0": Module.ConfigItem(
-                    "Min. and max. temperature",
-                    Module.ConfigItem.Type.MIN_MAX
-                ),
-                "temp1": Module.ConfigItem(
-                    "Something else?!?!?!",
-                    Module.ConfigItem.Type.MIN_MAX
-                ),
-                "temp2": Module.ConfigItem(
-                    "Something..",
-                    Module.ConfigItem.Type.ONE_VALUE
-                ),
-            }
-        )
-    ])
+    myapp.update_modules(
+        modules,
+    )
 
 
 Thread(target=test).start()
