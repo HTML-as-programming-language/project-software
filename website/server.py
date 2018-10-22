efc51e4a5cb898f3731147e1da1d58420c2369bb
timostrating <timostrating@home.nl>
2018-10-22 02:29:43 +0200

    live reloading

--- website/server.py
+++ website/server.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
-
-from flask import render_template, send_from_directory
 
 
 @app.route('/')
 def index():
-    return render_template('test2.html')
+    return render_template("index.html")
 
 @app.route('/<path:path>')
 def recourse(path):
     return send_from_directory('templates', path)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5003)
