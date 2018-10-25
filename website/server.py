1e8eb01c6b0b6041983d430a1c05990bc7a843c6
timostrating <timostrating@home.nl>
2018-10-26 00:02:34 +0200

    compile options now work

--- website/server.py
+++ website/server.py
@@ -3,6 +3,11 @@ import subprocess
 app = Flask(__name__)
 
 
+def compile_screen(screenname):
+    subprocess.call('./reactivate.sh', shell=True)
+    subprocess.call('./compile.sh ' + screenname, shell=True)
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -15,18 +20,21 @@ def recourse(path):
 
 @app.route('/screen/<path:path>')
 def screens(path):
-    if request.args.get("compile") == "true":
-        compile(path)
-
+    screenname = path.split(".")[0]
+    if request.args.get('compile') == "true":
+        compile_screen(screenname)
+        
     if "." in path:
-        return send_from_directory("templates/screens/"+path.split(".")[0], path)
+        return send_from_directory("templates/screens/"+screenname, path)
     else:
         return render_template("screens/"+path+"/"+path+".html")
 
 
+@app.route('/compile/<path:screenname>')
+def compile(screenname):
+    compile_screen(screenname)
+    return "oke"
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5003)
-
-
-def compile(path):
-    subprocess.call(['./compile.sh'])
