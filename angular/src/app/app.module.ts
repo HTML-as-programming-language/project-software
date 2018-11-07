40fc00dda6d801b6b1ee151b3263491062be7e3b
hilkojj <hilkojj@outlook.com>
2018-11-07 14:39:58 +0100

    bootstrap

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { MbsComponent } from './components/mbs/mbs.component';
@@ -12,7 +14,8 @@ import { MbsComponent } from './components/mbs/mbs.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
