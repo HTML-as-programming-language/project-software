c10b36474cb26defb09d9059bfe7b3f332d5f245
timostrating <timostrating@home.nl>
2018-11-07 14:47:49 +0100

    socket.io working

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, send_from_directory
+from flask_socketio import SocketIO, emit
+from threading import Thread, Event
+from time import sleep
+import random
 
-from flask import render_template, send_from_directory
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+socketio_thread = Thread()
+socketio_thread_stop_event = Event()
 
 
 @app.route('/')
@@ -12,6 +19,50 @@ def index():
 @app.route('/<path:path>')
 def recourse(path):
     return send_from_directory('templates', path)
+
+
+
+class RandomThread(Thread):
+    def __init__(self):
+        self.delay = 10
+        super(RandomThread, self).__init__()
+
+    def init(self):
+        while not socketio_thread_stop_event.isSet():
+            number = round(random.random()*10, 3)
+            socketio.emit('init', "nice")
+            sleep(self.delay)
+
+    def run(self):
+        self.init()
+
+
+@socketio.on('my event')
+def handle_message(message):
+    print('\n\n\nreceived message: ', message, "\n\n\n\n")
+
+
+@socketio.on('connect')
+def test_connect():
+    socketio.emit('init', """
+{"modules": [
+    {"data":{"hatch_status":8,"labelHatch open":"8%"},
+    "id":"devttyACM0",
+    "label":"/dev/ttyACM0",
+    "sensors":[{"data":{"label":"16C","temp":16},
+    "id":"0",
+    "label":"Temperature",
+    "settings":[{"id":"temp_threshold","label":"Temperature thresholds","max":30,"min":0,"subtype":"minmax","type":"int"}],"type":"TEMP"}]}
+]}""")
+
+    # need visibility of the global thread object
+    global socketio_thread
+    print('\n\nClient connected\n\n')
+    # Start the random number generator thread only if the thread has not been started before.
+    if not socketio_thread.isAlive():
+        print("\n\nStarting Thread\n\n")
+        socketio_thread = RandomThread()
+        socketio_thread.start()
 
 
 if __name__ == '__main__':
