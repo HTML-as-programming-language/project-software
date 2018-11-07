df79e6bcf0dc435f643041c93f63af6496d32cc0
timostrating <timostrating@home.nl>
2018-11-07 15:28:54 +0100

    more sockets

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO, emit
 from threading import Thread, Event
 from time import sleep
+from flask import jsonify
 import random
 
 app = Flask(__name__)
@@ -20,26 +21,30 @@ def index():
 def recourse(path):
     return send_from_directory('templates', path)
 
+@app.route('/api')
+def api():
+    return jsonify({"data": "new"})
+
 
 
 class RandomThread(Thread):
     def __init__(self):
-        self.delay = 10
+        self.delay = 1
         super(RandomThread, self).__init__()
 
     def init(self):
         while not socketio_thread_stop_event.isSet():
-            number = round(random.random()*10, 3)
-            socketio.emit('init', "nice")
+            n = str(round(random.random()*10, 3))
+            socketio.emit('update', '{"modules": [{"data":{"hatch_status":'+n+',"labelHatch open":"'+n+'%"}, "id":"devttyACM0", "label":"/dev/ttyACM0", "sensors":[{"data":{"label":"'+n+'C","temp":'+n+'}, "id":"0", "label":"Temperature", "settings":[{"id":"temp_threshold","label":"Temperature thresholds","max":'+n+',"min":0,"subtype":"minmax","type":"int"}],"type":"TEMP"}]}]}')
             sleep(self.delay)
 
     def run(self):
         self.init()
 
 
-@socketio.on('my event')
-def handle_message(message):
-    print('\n\n\nreceived message: ', message, "\n\n\n\n")
+# @socketio.on('my event')
+# def handle_message(message):
+#     print('\n\n\nreceived message: ', message, "\n\n\n\n")
 
 
 @socketio.on('connect')
