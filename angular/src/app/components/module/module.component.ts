dd25e65c2f54821f237dc5a89bf5d360629defba
hilkojj <hilkojj@outlook.com>
2018-11-09 00:47:30 +0100

    min max settings

--- angular/src/app/components/module/module.component.ts
+++ angular/src/app/components/module/module.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModuleService } from 'src/app/services/module.service';
 import { ActivatedRoute } from '@angular/router';
-import { Module } from 'src/app/models/module';
+import { Module, Setting } from 'src/app/models/module';
 
 @Component({
     selector: 'app-module',
@@ -9,6 +9,8 @@ import { Module } from 'src/app/models/module';
     styleUrls: ['./module.component.scss']
 })
 export class ModuleComponent implements OnInit {
+
+    settings: Setting[];
 
     constructor(
         public moduleService: ModuleService,
@@ -20,17 +22,43 @@ export class ModuleComponent implements OnInit {
     }
 
     get module(): Module {
-        return this.moduleService.getById(this.route.snapshot.params.id);
+        var module = this.moduleService.getById(this.route.snapshot.params.id);
+
+        if (!this.settings && module) {
+            this.settings = [];
+            for (var sensor of module.sensors)
+                for (var sett of sensor.settings)
+                        this.settings.push(JSON.parse(JSON.stringify(sett)));
+        }
+
+        return module;
     }
 
     get data() {
         var m = this.module;
         if (!m) return;
         var data = [];
-        for (var key in m.data) 
-            if (typeof key == "string" && key.startsWith("label")) 
-                data.push([key.substr(5), m.data[key]]);
+        const getData = (obj, startWithLabel) => {
+            if (!obj) return;
+            for (var key in obj) 
+                if (typeof key == "string" && (!startWithLabel || key.startsWith("label"))) 
+                    data.push([startWithLabel ? key.substr(5) : key, obj[key]]);
+        }
+        getData(m.data, true);
+        for (var sensor of m.sensors) getData(sensor.data, false);
         return data;
+    }
+
+    minRange(sett: Setting): number {
+        if (sett.id == "temp_threshold")
+            return -30;
+        return 0;
+    }
+
+    maxRange(sett: Setting): number {
+        if (sett.id == "temp_threshold")
+            return 50;
+        return 100;
     }
 
 }
