c7b99f2d145f46b05a5204a27d1828aa196a655a
hilkojj <hilkojj@outlook.com>
2018-11-09 15:51:29 +0100

    very annoying voice

--- angular/src/app/components/modules-list/modules-list.component.ts
+++ angular/src/app/components/modules-list/modules-list.component.ts
@@ -13,6 +13,28 @@ export class ModulesListComponent implements OnInit {
     ) { }
 
     ngOnInit() {
+
+        setInterval(() => {
+
+            if ('speechSynthesis' in window && this.modulesService.modules.length == 0) {
+                var msg = new SpeechSynthesisUtterance();
+                var voices = window.speechSynthesis.getVoices();
+                if (voices.length > 0) {
+                    console.log("Your browser supports " + voices.length + " voices");
+                    console.log(voices);
+                    msg.voice = voices.filter(function (voice) { return voice.lang == "nl"; })[1];
+                }
+                msg["voiceURI"] = 'native';
+                msg.volume = 1; // 0 to 1
+                msg.rate = 0.8; // 0.1 to 10
+                msg.pitch = 1; //0 to 2
+                msg.text = "Sluit uw module aan a.u.b.";
+                msg.lang = "nl";
+                speechSynthesis.speak(msg);
+            }
+
+        }, 10000);
+
     }
 
 }
