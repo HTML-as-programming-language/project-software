86e9b8e20b0f3f808d4a3ef1eb0d7886dbb7f319
timostrating <timostrating@home.nl>
2018-11-09 03:01:32 +0100

    header and footer

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { ModulesListComponent } from './components/modules-list/modules-list.component';
 import { ModuleComponent } from './components/module/module.component';
+import { HeaderComponent } from './ui/header/header.component';
+import { FooterComponent } from './ui/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ModulesListComponent,
-    ModuleComponent
+    ModuleComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
